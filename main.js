46d632affdcdf542659a1322415a6da9285468e4
MARCOSANZANELLO <110115385+MARCOSANZANELLO@users.noreply.github.com>
2023-10-06 09:33:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,4 +2,28 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom')
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomClap;
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splas').play();
+}
+
+document.querySelector('.tecla_splsh').onclick = tocaSomSplash;
